c9edc5c9df7a8289b53990aaefe0d3cde0694fcc
tiagoschad <tiagoschad@gmail.com>
2022-04-26 15:28:55 -0300

    botao delete

--- lista.js
+++ lista.js
@@ -1,45 +1,36 @@
-function Submit(){
-    const valor = document.querySelector('[data-form-input]')
-    console.log(valor.value)
-    const novo_item_lista = document.createElement("li")
-    const lista = document.querySelector('[data-task]')
-
-    novo_item_lista.innerHTML = valor.value
-    lista.appendChild (novo_item_lista)
+function CriarBotaoEdit(){
+    const botao_edit = document.createElement('button')
+    botao_edit.classList.add("btn btn-info")
+    botao_edit.innerHTML = "Edit"
+    botao_edit.type = "button"
 }
 
+function DeleteItem(){
+    console.log(this.parentElement)
+    this.parentElement.remove()
+}
 
+function CriarBotaoDelete(){
+    const botao_delete =  document.createElement('button')
+    botao_delete.classList.add("btn", "btn-danger")
+    botao_delete.innerHTML = "Delete"
+    botao_delete.type = "button"
+    botao_delete.addEventListener("click", DeleteItem);
 
+    return botao_delete
+}
 
+function Submit(){
+    const lista = document.querySelector('[data-task]')
+    const valor = document.querySelector('[data-form-input]')
 
+    const novo_item_lista = document.createElement("li")
+    novo_item_lista.className = "list-group-item";
 
+    novo_item_lista.innerHTML = valor.value
+ 
+    novo_item_lista.appendChild(CriarBotaoDelete())
+    lista.appendChild (novo_item_lista)
 
-
-
-
-
-
-
-// function myFunction(){
-//     console.log("teste inicial");
-// }
-
-// function addItem(){
-//     const inputItem = document.querySelector('[data-form-input]')
-//     const valorItem = inputItem.value
-
-//     const listaDeItems = document.querySelector('[data-task]')
-
-//     novaLabel = document.createElement('label')
-//     novaLabel.innerText = valorItem
-    
-//     novoItem = document.createElement('li')
-
-//     // novoItem.appendChild(criarBotaoConcluir())
-//     novoItem.appendChild(novaLabel)
-//     // novoItem.appendChild(criarBotaoDelete())
-
-//     listaDeItems.appendChild(novoItem)
-
-//     inputItem.value = ""
-//}
+    document.getElementById("item").value = ""
+ }
